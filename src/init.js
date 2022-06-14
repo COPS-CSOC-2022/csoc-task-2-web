3540b46b25187a89bc293e51dc01a4965cb20e2f
Ujjawal <umodi2003@gmail.com>
2022-06-14 18:22:08 +0530

    Tasks completed

--- src/init.js
+++ src/init.js
@@ -5,6 +5,46 @@ function getTasks() {
     /***
      * @todo Fetch the tasks created by the user and display them in the dom.
      */
+    const header={
+        'Authorization': `Token ${window.localStorage.getItem('token')}`,
+        'Content-Type': "application/json"
+    }
+
+    axios({
+        url: API_BASE_URL + 'todo/',
+        method: 'get',
+        headers: header
+    }).then(function({data, status}) {
+     
+        let todo =document.querySelector(".todo-available-tasks")
+        todo.innerHTML=`<span class="badge badge-primary badge-pill todo-available-tasks-text"> Available Tasks </span>`
+        for (let element of data){
+        todo.innerHTML+=
+        `   
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <input id="input-button-${element.id}" type="text" class="form-control todo-edit-task-input hideme" placeholder="Edit The Task" value=${element.title}>
+                    <div id="done-button-${element.id}"  class="input-group-append hideme">
+                        <button class="btn btn-outline-secondary todo-update-task" type="button" onclick="updateTask(${element.id})">Done</button>
+                    </div>
+                    <div id="task-${element.id}" class="todo-task">
+                        ${element.title}
+                    </div>
+
+                    <span id="task-actions-${element.id}">
+                        <button style="margin-right:5px;" type="button" onclick="editTask(${element.id})"
+                            class="btn btn-outline-warning">
+                            <img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486663/CSOC/edit.png"
+                                width="18px" height="20px">
+                        </button>
+                        <button type="button" class="btn btn-outline-danger" onclick="deleteTask(${element.id})">
+                            <img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486661/CSOC/delete.svg"
+                                width="18px" height="22px">
+                        </button>
+                    </span>
+            </li>
+        `
+        }
+    })
 }
 
 axios({
@@ -18,3 +58,5 @@ axios({
   document.getElementById('profile-name').innerHTML = data.name;
   getTasks();
 })
+
+window.getTasks=getTasks
