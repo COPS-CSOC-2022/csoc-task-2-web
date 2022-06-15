9746a5a607ee4c2f2544b3df4e4bfaa75b344fba
nikhil <nikhil10july123@gmail.com>
2022-06-15 10:48:49 +0530

    No Auth Required Task Done Completely

--- src/no_auth_required.js
+++ src/no_auth_required.js
@@ -1,3 +1,13 @@
 /***
  * @todo Redirect the user to main page if token is present.
  */
+
+ const token = localStorage.getItem('token');
+
+ if( token !=null)
+ {
+     window.location.href="/";
+ }
+
+
+
