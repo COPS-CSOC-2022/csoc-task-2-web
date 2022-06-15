f3c24fdedb40ef4afad1799d0ff4b78bad0e2bbb
nikhil <nikhil10july123@gmail.com>
2022-06-15 10:49:49 +0530

    made initially changes

--- src/init.js
+++ src/init.js
@@ -1,11 +1,26 @@
 import axios from 'axios';
+import { addNewData } from './main';
 const API_BASE_URL = 'https://todo-app-csoc.herokuapp.com/';
 
 function getTasks() {
-    /***
-     * @todo Fetch the tasks created by the user and display them in the dom.
-     */
+    axios({
+        headers: {
+            Authorization: 'Token ' + localStorage.getItem('token'),
+        },
+        url: API_BASE_URL + 'todo/',
+        method: 'get',
+    }).then(function (res) {
+        const array = res.data;
+        array.map((data) => {
+            addNewData(data);
+        })
+    }).catch((e) => {
+        console.log(e);
+    })
 }
+
+
+
 
 axios({
     headers: {
@@ -13,8 +28,14 @@ axios({
     },
     url: API_BASE_URL + 'auth/profile/',
     method: 'get',
-}).then(function({data, status}) {
-  document.getElementById('avatar-image').src = 'https://ui-avatars.com/api/?name=' + data.name + '&background=fff&size=33&color=007bff';
-  document.getElementById('profile-name').innerHTML = data.name;
-  getTasks();
+}).then(function ({ data, status }) {
+    document.getElementById('avatar-image').src = 'https://ui-avatars.com/api/?name=' + data.name + '&background=fff&size=33&color=007bff';
+    document.getElementById('profile-name').innerHTML = data.name;
+    getTasks();
 })
+
+export{getTasks};
+
+
+
+
