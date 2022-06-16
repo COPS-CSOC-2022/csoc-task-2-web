3fa8e7e99e61a243fff7a9f93abb1e8b44094a10
nikhil <nikhil10july123@gmail.com>
2022-06-16 17:17:26 +0530

    init.js done with comments

--- src/init.js
+++ src/init.js
@@ -1,19 +1,36 @@
+// importing axios 
 import axios from 'axios';
+
+// importing addNewData from main.js 
 import { addNewData } from './main';
+
+// storing base url of todo API
 const API_BASE_URL = 'https://todo-app-csoc.herokuapp.com/';
 
+
+// function to get task of user and display it using addNewData function 
 function getTasks() {
+
+    // fetching api using axios 
+    // get request to get task of users 
     axios({
+
+        // providing headers 
         headers: {
             Authorization: 'Token ' + localStorage.getItem('token'),
         },
         url: API_BASE_URL + 'todo/',
         method: 'get',
     }).then(function (res) {
+
+        // storing data in array 
         const array = res.data;
+
+        // mapping into array and calling addNewData function
         array.map((data) => {
             addNewData(data);
         })
+
     }).catch((e) => {
         console.log(e);
     })
@@ -34,7 +51,6 @@ axios({
     getTasks();
 })
 
-export{getTasks};
 
 
 
