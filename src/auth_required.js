3b83dbbac110c760f64697510d613846b68e46dc
nikhil <nikhil10july123@gmail.com>
2022-06-16 17:09:55 +0530

    auth_required.js completed with comments

--- src/auth_required.js
+++ src/auth_required.js
@@ -1,5 +1,9 @@
+
+// taking token from local storage
 const token = localStorage.getItem('token');
 
+
+// if token is not there then redirect the user to login page 
 if(!token)
 {
     window.location.href = '/login/';
