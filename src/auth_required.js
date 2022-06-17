da78390540b22d95ce332d1fa3a0e6a0991abcb3
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2022-06-17 23:11:48 +0530

    Completed all functions

--- src/auth_required.js
+++ src/auth_required.js
@@ -1,3 +1,8 @@
 /***
- * @todo Redirect the user to login page if token is not present.
+ * @done 
+ * Redirect the user to login page if token is not present.
  */
+
+if (localStorage.token === undefined) {
+    window.location.href = 'login/';
+} 
