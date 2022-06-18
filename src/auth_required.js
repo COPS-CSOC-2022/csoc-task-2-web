31ebf5aa73060ecc87734a7546247570a0d512fc
shrivardha <patil.shrivardhanc.cd.cse20@itbhu.ac.in>
2022-06-19 00:34:55 +0530

    To-do app

--- src/auth_required.js
+++ src/auth_required.js
@@ -1,3 +1,10 @@
 /***
  * @todo Redirect the user to login page if token is not present.
  */
+ function login_required(){
+    const token = localStorage.getItem('token');
+    if(!token){
+        window.location.href = '/login/'
+    }
+ }
+ login_required()
