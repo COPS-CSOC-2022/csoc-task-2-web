013a40df4838ac0348808340bded699fb613cd00
Shuchi17 <bhatt.shuchikeyur.chy20@itbhu.ac.in>
2022-06-18 23:10:39 +0530

    CSOC Week 2

--- src/auth_required.js
+++ src/auth_required.js
@@ -1,3 +1,12 @@
 /***
  * @todo Redirect the user to login page if token is not present.
  */
+ function redirectLogin () {
+    const token = localStorage.getItem('token');
+    if(!token)
+    {
+        window.location.href = '/login/';
+    }
+}
+
+redirectLogin(); 
--- src/no_auth_required.js
+++ src/no_auth_required.js
@@ -1,3 +1,12 @@
 /***
  * @todo Redirect the user to main page if token is present.
  */
+ function redirectMain () {
+    const token = localStorage.getItem('token');
+    if(token)
+    {
+        window.location.href = '/';
+    }
+}
+
+redirectMain(); 
