47c3926539a47390d89afb4f9e68ccb76e2ad486
ayushanand16 <ayush.anand.cse21@itbhu.ac.in>
2022-06-18 17:44:46 +0530

    Completed All Tasks

--- src/init.js
+++ src/init.js
@@ -1,15 +1,33 @@
 import axios from 'axios';
+import {editTask, updateTask, deleteTask, TodoDisplayer} from './main';
+
 const API_BASE_URL = 'https://todo-app-csoc.herokuapp.com/';
 
 function getTasks() {
     /***
      * @todo Fetch the tasks created by the user and display them in the dom.
      */
+    axios({
+        headers : {
+            Authorization : "Token " + localStorage.getItem("Token")
+        },
+        url: API_BASE_URL + "todo/",
+        method : "get"
+    }).then(function (res){
+        const {data,status} = res;
+
+        for(let i=0 ; i<data.length ; i++){
+            const newTaskid = data[i].id;
+            const task_title = data[i].title;
+            TodoDisplayer(newTaskid,task_title);
+        }
+
+    })
 }
 
 axios({
     headers: {
-        Authorization: 'Token ' + localStorage.getItem('token'),
+        Authorization: 'Token ' + localStorage.getItem('Token'),
     },
     url: API_BASE_URL + 'auth/profile/',
     method: 'get',
@@ -18,3 +36,4 @@ axios({
   document.getElementById('profile-name').innerHTML = data.name;
   getTasks();
 })
+
