093ac1578c9a3b75d778f214da80fdb883af96bc
Soustab Haldar <soustabhaldar10@gmail.com>
2022-06-19 12:22:20 +0530

    Week 2

--- src/auth_required.js
+++ src/auth_required.js
@@ -1,3 +1,8 @@
 /***
  * @todo Redirect the user to login page if token is not present.
  */
+
+const userToken = localStorage.getItem('token');
+if (userToken == null || userToken.length == 0 || userToken === undefined) {
+    window.location.href = 'login/';
+}
