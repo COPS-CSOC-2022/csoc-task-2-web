0bc1bbfb74eae453f7ab31ffb550f6afee8677f6
Suhani Garg <suhanigarg2002@gmail.com>
2022-06-19 13:04:42 +0530

    Added functionality

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import axios from 'axios';
+import { displayTask, getTasks } from './init';
+window.deleteTask = deleteTask;
+window.updateTask = updateTask;
+window.editTask = editTask;
+const taskList = document.getElementById("taskList");
+
+const registerBtn = document.getElementById("registerBtn");
+if (registerBtn) registerBtn.onclick = register;
+const loginBtn = document.getElementById("loginBtn");
+if (loginBtn) loginBtn.onclick = login;
+const logoutButton = document.getElementById("logoutButton");
+if (logoutButton) logoutButton.onclick = logout;
+const addTaskButton = document.getElementById("addTaskButton");
+if (addTaskButton) addTaskButton.onclick = addTask;
+const searchTaskButton = document.getElementById("searchTaskButton");
+if (searchTaskButton) searchTaskButton.onclick = searchTask;
+
+
 function displaySuccessToast(message) {
     iziToast.success({
         title: 'Success',
@@ -47,7 +65,7 @@ function register() {
     const password = document.getElementById('inputPassword').value;
 
     if (registerFieldsAreValid(firstName, lastName, email, username, password)) {
-        displayInfoToast("Please wait...");
+        displayInfoToast("Loading...");
 
         const dataForApiRequest = {
             name: firstName + " " + lastName,
@@ -70,40 +88,174 @@ function register() {
 }
 
 function login() {
+    
     /***
      * @todo Complete this function.
      * @todo 1. Write code for form validation.
      * @todo 2. Fetch the auth token from backend, login and direct user to home page.
      */
+
+     const username = document.getElementById('inputUsername').value.trim();
+     const password = document.getElementById('inputPassword').value;
+ 
+     if (username == '' || password == '') {
+         displayErrorToast("Please enter the required fields.");
+         return;
+     }
+     displayInfoToast("Loading...");
+     const dataForApiRequest = {
+         username: username,
+         password: password
+     }
+     axios({
+         url: API_BASE_URL + 'auth/login/',
+         method: 'POST',
+         data: dataForApiRequest,
+     }).then(function ({ data, status }) {
+         displaySuccessToast("Login Successful...");
+         localStorage.setItem('token', data.token);
+         window.location.href = '/';
+     }).catch(function (err) {
+         displayErrorToast("The credentials used are invalid.");
+         document.getElementById('inputUsername').value = '';
+         document.getElementById('inputPassword').value = '';
+     })
+ 
 }
 
 function addTask() {
+
     /**
      * @todo Complete this function.
      * @todo 1. Send the request to add the task to the backend server.
      * @todo 2. Add the task in the dom.
      */
+
+     const task = document.getElementById('inputTask').value.trim();
+
+     if (task == '') {
+         displayErrorToast("Task cannot be empty....");
+         return;
+     }
+ 
+     displayInfoToast("Loading...");
+ 
+     const dataForApiRequest = {
+         title: task
+     }
+     const headersForApiRequest = {
+         Authorization: 'Token ' + localStorage.getItem('token')
+     }
+ 
+     axios({
+         headers: headersForApiRequest,
+         url: API_BASE_URL + 'todo/create/',
+         method: 'POST',
+         data: dataForApiRequest,
+     }).then(function ({ data, status }) {
+         displaySuccessToast("Task added successfully...");
+         document.getElementById('inputTask').value = '';
+         getTasks();
+     }).catch(function (err) {
+         displayErrorToast("Unable to add task. Please try again...");
+     })
 }
 
-function editTask(id) {
+export function editTask(id) {
     document.getElementById('task-' + id).classList.add('hideme');
     document.getElementById('task-actions-' + id).classList.add('hideme');
     document.getElementById('input-button-' + id).classList.remove('hideme');
     document.getElementById('done-button-' + id).classList.remove('hideme');
 }
 
-function deleteTask(id) {
+export function deleteTask(id) {
     /**
      * @todo Complete this function.
      * @todo 1. Send the request to delete the task to the backend server.
      * @todo 2. Remove the task from the dom.
      */
+
+     displayInfoToast("Loading...");
+
+     const headersForApiRequest = {
+         Authorization: 'Token ' + localStorage.getItem('token')
+     }
+ 
+     axios({
+         headers: headersForApiRequest,
+         url: API_BASE_URL + 'todo/' + id + '/',
+         method: 'DELETE',
+     }).then(function ({ data, status }) {
+         displaySuccessToast("Task deleted successfully...");
+         getTasks();
+     }).catch(function (err) {
+         displayErrorToast("Unable to delete task. Please try again...");
+     })
 }
 
-function updateTask(id) {
+export function updateTask(id) {
     /**
      * @todo Complete this function.
      * @todo 1. Send the request to update the task to the backend server.
      * @todo 2. Update the task in the dom.
      */
+
+     const newTask = document.getElementById("input-button-" + id).value.trim();
+    const taskItem = document.getElementById("task-" + id);
+
+    if (newTask==""){
+        displayErrorToast("Task title cannot be empty");
+        return;
+    }
+
+    const dataForApiRequest = {
+        title: newTask
+    }
+    const headersForApiRequest = {
+        Authorization: 'Token ' + localStorage.getItem('token')
+    }
+
+    axios({
+        headers: headersForApiRequest,
+        url: API_BASE_URL + 'todo/' + id + '/',
+        method: 'PUT',
+        data: dataForApiRequest,
+    }).then(function ({ data, status }) {
+        taskItem.textContent=data.title;
+        editTask(data.id);
+        displaySuccessToast("Task updated successfully...");
+        getTasks();
+    }).catch(function (err) {
+        displayErrorToast("Unable to update task. Please try again...");
+    })
+}
+
+function searchTask(){
+    const task = document.getElementById('searchTask').value.trim();
+
+    if (task == '') {
+        displayErrorToast("Task cannot be empty....");
+        return;
+    }
+
+    displayInfoToast("Loading...");
+
+    const headersForApiRequest = {
+        Authorization: 'Token ' + localStorage.getItem('token')
+    }
+
+    axios({
+        headers: headersForApiRequest,
+        url: API_BASE_URL + 'todo/',
+        method: 'GET',
+    }).then(function ({ data, status }) {
+        console.log(data);
+        for (var index = 0; index < data.length; index++) if (data[index].title == task){
+            taskList.innerHTML = "";
+            displaySuccessToast("Task found...");
+            displayTask(data[index].id, data[index].title);
+            return;
+        }
+        displayErrorToast("The specified task could not be found...")
+    })
 }
