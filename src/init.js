9f0c730ba5fee5b9993d31ac5b19a6b660a6da30
1eque <vanithapar17@gmail.com>
2022-06-19 23:45:09 +0530

    final

--- src/init.js
+++ src/init.js
@@ -5,6 +5,81 @@ function getTasks() {
     /***
      * @todo Fetch the tasks created by the user and display them in the dom.
      */
+    console.log('getTasks');
+    //get token from local storage for authentication
+    const token = localStorage.getItem('token');
+    axios({
+        url: API_BASE_URL + 'todo/',
+        method: 'get',
+        headers: {
+            'Authorization': 'Token ' + token
+        },
+    }).then(function ({ data, status }) {
+        console.log(data);
+        const list_group = document.querySelector('.list-group');
+        list_group.textContent = '';
+        data.forEach(d => {
+            const tasks_container = document.createElement('li');
+            tasks_container.classList.add('list-group-item');
+            tasks_container.classList.add('d-flex');
+            tasks_container.classList.add('justify-content-between');
+            tasks_container.classList.add('align-items-center');
+            tasks_container.id = d.id;
+            // add a input field to edit the task
+            const input_field = document.createElement('input');
+            input_field.classList.add('form-control');
+            input_field.classList.add('todo-edit-task-input');
+            input_field.classList.add('hideme');
+            input_field.id = 'input-button-' + d.id;
+            input_field.placeholder = 'Edit The Task';
+            input_field.type = 'text';
+            // create a div to store button
+            const button_container = document.createElement('div');
+            button_container.classList.add('input-group-append');
+            button_container.classList.add('hideme');
+            button_container.id = 'done-button-' + d.id;
+            // create a div and done button inside it to save edited task
+            const done_button = document.createElement('button');
+            done_button.classList.add('btn');
+            done_button.classList.add('btn-success');
+            done_button.classList.add('todo-update-task');
+            done_button.type = 'button';
+            done_button.setAttribute('onclick', 'updateTask(' + d.id + ')');
+            done_button.innerHTML = 'Done';
+            button_container.append(done_button);
+            const task_name = document.createElement('div');
+            task_name.classList.add('todo-task');
+            task_name.innerHTML = d.title;
+            task_name.id = 'task-' + d.id;
+            const span = document.createElement('span');
+            const btn_1 = document.createElement('button');
+            btn_1.classList.add('btn');
+            btn_1.classList.add('btn-outline-warning');
+            btn_1.style.marginRight = '10px';
+            btn_1.addEventListener('click', () => {
+                editTask(d.id);
+            }
+            );
+            const btn_2 = document.createElement('button');
+            btn_2.classList.add('btn');
+            btn_2.classList.add('btn-outline-danger');
+            btn_2.addEventListener('click', () => { deleteTask(d.id) });
+            span.append(btn_1, btn_2);
+            span.id = 'task-actions-' + d.id;
+            tasks_container.append(input_field, button_container, task_name, span);
+            list_group.append(tasks_container);
+            const img_1 = document.createElement('img');
+            img_1.src = 'https://res.cloudinary.com/nishantwrp/image/upload/v1587486663/CSOC/edit.png';
+            img_1.width = '18';
+            img_1.height = '20';
+            btn_1.appendChild(img_1);
+            const img_2 = document.createElement('img');
+            img_2.src = 'https://res.cloudinary.com/nishantwrp/image/upload/v1587486661/CSOC/delete.svg';
+            img_2.width = '18';
+            img_2.height = '22';
+            btn_2.appendChild(img_2);
+        });
+    });
 }
 
 axios({
@@ -18,3 +93,85 @@ axios({
   document.getElementById('profile-name').innerHTML = data.name;
   getTasks();
 })
+
+
+function getSearchedTasks(data) {
+    console.log(data);
+    const list_group = document.querySelector('.list-group');
+    list_group.textContent = '';
+    data.forEach(d => {
+        const tasks_container = document.createElement('li');
+        tasks_container.classList.add('list-group-item');
+        tasks_container.classList.add('d-flex');
+        tasks_container.classList.add('justify-content-between');
+        tasks_container.classList.add('align-items-center');
+        tasks_container.id = d.id;
+        const input_field = document.createElement('input');
+        input_field.classList.add('form-control');
+        input_field.classList.add('todo-edit-task-input');
+        input_field.classList.add('hideme');
+        input_field.id = 'input-button-' + d.id;
+        input_field.placeholder = 'Edit The Task';
+        input_field.type = 'text';
+        const button_container = document.createElement('div');
+        button_container.classList.add('input-group-append');
+        button_container.classList.add('hideme');
+        button_container.id = 'done-button-' + d.id;
+        const done_button = document.createElement('button');
+        done_button.classList.add('btn');
+        done_button.classList.add('btn-outline-secondary');
+        done_button.classList.add('todo-update-task');
+        done_button.type = 'button';
+        done_button.setAttribute('onclick', 'updateTask(' + d.id + ')');
+        done_button.innerHTML = 'Done';
+        button_container.append(done_button);
+        const task_name = document.createElement('div');
+        task_name.classList.add('todo-task');
+        task_name.innerHTML = d.title;
+        task_name.id = 'task-' + d.id;
+        const span = document.createElement('span');
+        const btn_1 = document.createElement('button');
+        btn_1.classList.add('btn');
+        btn_1.classList.add('btn-outline-warning');
+        btn_1.style.marginRight = '10px';
+        btn_1.addEventListener('click', () => {
+            editTask(d.id);
+        }
+        );
+        const btn_2 = document.createElement('button');
+        btn_2.classList.add('btn');
+        btn_2.classList.add('btn-outline-danger');
+        btn_2.addEventListener('click', () => { deleteTask(d.id) });
+        span.append(btn_1, btn_2);
+        span.id = 'task-actions-' + d.id;
+        tasks_container.append(input_field, button_container, task_name, span);
+        list_group.append(tasks_container);
+        const img_1 = document.createElement('img');
+        img_1.src = 'https://res.cloudinary.com/nishantwrp/image/upload/v1587486663/CSOC/edit.png';
+        img_1.width = '18';
+        img_1.height = '20';
+        btn_1.appendChild(img_1);
+        const img_2 = document.createElement('img');
+        img_2.src = 'https://res.cloudinary.com/nishantwrp/image/upload/v1587486661/CSOC/delete.svg';
+        img_2.width = '18';
+        img_2.height = '22';
+        btn_2.appendChild(img_2);
+    });
+}
+
+
+
+axios({
+    headers: {
+        Authorization: 'Token ' + localStorage.getItem('token'),
+    },
+    url: API_BASE_URL + 'auth/profile/',
+    method: 'get',
+}).then(function ({ data }) {
+    document.getElementById('avatar-image').src = 'https://ui-avatars.com/api/?name=' + data.name + '&background=fff&size=33&color=007bff';
+    document.getElementById('profile-name').innerHTML = data.name;
+    getTasks();
+})
+//export getTasks() for use in main.js
+export { getTasks };
+export { getSearchedTasks};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import axios from 'axios';
+//import getTasks from init.js using import
+import {getTasks} from './init';
+import {getSearchedTasks} from './init';
+
+window.register = register;
+window.login = login;
+window.addTask = addTask;
+window.logout=logout;
+window.deleteTask=deleteTask;
+window.editTask=editTask;
+window.updateTask=updateTask;
+window.searchTask=searchTask;
+
 function displaySuccessToast(message) {
     iziToast.success({
         title: 'Success',
@@ -60,14 +73,15 @@ function register() {
             url: API_BASE_URL + 'auth/register/',
             method: 'post',
             data: dataForApiRequest,
-        }).then(function({data, status}) {
-          localStorage.setItem('token', data.token);
-          window.location.href = '/';
-        }).catch(function(err) {
-          displayErrorToast('An account using same email or username is already created');
+        }).then(function ({ data, status }) {
+            localStorage.setItem('token', data.token);
+            window.location.href = '/';
+        }).catch(function (err) {
+            displayErrorToast('An account using same email or username is already created');
         })
     }
 }
+
 
 function login() {
     /***
@@ -75,7 +89,28 @@ function login() {
      * @todo 1. Write code for form validation.
      * @todo 2. Fetch the auth token from backend, login and direct user to home page.
      */
+    //  const username = username;
+    //  const password = password;
+
+    const username = document.getElementById('inputUsername').value.trim();
+    const password = document.getElementById('inputPassword').value;
+
+    const dataForApiRequest = {
+        username: username,
+        password: password
+    }
+    axios({
+        url: API_BASE_URL + 'auth/login/',
+        method: 'post',
+        data: dataForApiRequest,
+    }).then(function ({ data, status }) {
+        localStorage.setItem('token', data.token);
+        window.location.href = '/';
+    }).catch(function (err) {
+        displayErrorToast('Account does not exist, please register first');
+    })
 }
+
 
 function addTask() {
     /**
@@ -83,13 +118,79 @@ function addTask() {
      * @todo 1. Send the request to add the task to the backend server.
      * @todo 2. Add the task in the dom.
      */
+
+    const todoInput = document.querySelector("input");
+    const todoCollection = document.querySelector(".todo-available-tasks");
+
+
+    const dataForApiRequest = {
+        title: todoInput.value,
+    }
+    //check whether the task is empty or not
+    if (todoInput.value === '') {
+        displayErrorToast("Please enter a task");
+    }
+    else {
+
+
+
+        //get token from local storage for authentication
+        const token = localStorage.getItem('token');
+        axios({
+            url: API_BASE_URL + 'todo/create/',
+            method: 'post',
+            headers: {
+                'Authorization': 'Token ' + token
+            },
+            data: dataForApiRequest,
+        }).then(function ({ data, status }) {
+          console.log(data);
+          getTasks()
+            todoInput.value = '';
+        }).catch(function (err) {
+            displayErrorToast('An error occured while adding the task');
+
+        }
+        )
+
+    }
+
 }
+// if (todoInput.value === "") {
+//     alert("enter something!");
+// } else {
+//     const li = document.createElement("li");
+//     const todoTitle = document.createElement("span");
+//     li.classList.add("todo-available-tasks-text");
+//     todoTitle.classList.add("list-items");
+//     todoTitle.innerText = todoInput.value;
+
+//     li.appendChild(todoTitle);
+//     todoCollection.appendChild(li);
+
+// }
+
+
+// axios({
+//     url: API_BASE_URL + 'todo/create/',
+//     method: 'post',
+//     data: dataForApiRequest,
+// }).then(function ({ data, status }) {
+//     localStorage.setItem('token', data.token);
+//     window.location.href = '/';
+// }).catch(function (err) {
+//     displayErrorToast('please login');
+// })
+
 
 function editTask(id) {
     document.getElementById('task-' + id).classList.add('hideme');
     document.getElementById('task-actions-' + id).classList.add('hideme');
     document.getElementById('input-button-' + id).classList.remove('hideme');
     document.getElementById('done-button-' + id).classList.remove('hideme');
+
+    
+
 }
 
 function deleteTask(id) {
@@ -98,6 +199,35 @@ function deleteTask(id) {
      * @todo 1. Send the request to delete the task to the backend server.
      * @todo 2. Remove the task from the dom.
      */
+
+
+    const token = localStorage.getItem('token');
+    axios({
+        url: API_BASE_URL + 'todo/' + id + '/',
+        method: 'delete',
+        headers: {
+            'Authorization': 'Token ' + token
+        },
+    }).then(function ({ data, status }) {
+        // getTasks()
+        //success toast
+        displaySuccessToast('Task deleted successfully');
+        //select the task from the dom
+        const task = document.getElementById(id);
+        //remove the task from the dom
+        task.remove();
+
+    }).catch(function (err) {
+        displayErrorToast('An error occured while deleting the task');
+    
+    }
+    )
+    //delete the task from the dom
+    document.getElementById('task-' + id).classList.add('hideme');
+    document.getElementById('task-actions-' + id).classList.add('hideme');
+    document.getElementById('input-button-' + id).classList.remove('hideme');
+    document.getElementById('done-button-' + id).classList.remove('hideme');
+
 }
 
 function updateTask(id) {
@@ -106,4 +236,78 @@ function updateTask(id) {
      * @todo 1. Send the request to update the task to the backend server.
      * @todo 2. Update the task in the dom.
      */
+   //update the edited task and save to dom
+   console.log(id);
+    const todoInput = document.getElementById("input-button-" + id);
+    console.log(todoInput);
+    const todoCollection = document.querySelector(".todo-available-tasks");
+    const token = localStorage.getItem('token');
+    const dataForApiRequest = {
+        title: todoInput.value,
+    }
+    console.log(todoInput.value)
+    axios({
+        url: API_BASE_URL + 'todo/' + id + '/',
+        method: 'put',
+        headers: {
+            'Authorization': 'Token ' + token
+        },
+        data: dataForApiRequest,
+    }).then(function ({ data, status }) {
+        getTasks()
+        //success toast
+        displaySuccessToast('Task updated successfully');
+
+    }).catch(function (err) {
+        displayErrorToast('An error occured while updating the task');
+    }   
+    )
+//save the edited task to dom
+    document.getElementById('task-' + id).classList.remove('hideme');
+    document.getElementById('task-actions-' + id).classList.remove('hideme');
+    document.getElementById('input-button-' + id).classList.add('hideme');
+    document.getElementById('done-button-' + id).classList.add('hideme');
+
+    //show the updated tasks in the dom
+    // const li = document.createElement("li");
+    // const todoTitle = document.createElement("span");
+    // li.classList.add("todo-available-tasks-text");
+    // todoTitle.classList.add("list-items");
+    // todoTitle.innerText = todoInput.value;
+
+   
+
+
+        
 }
+//call a function on click of search button
+function searchTask()   {
+    displayInfoToast("Searching Task...");
+
+
+    const token = localStorage.getItem('token');
+    axios({
+        url: API_BASE_URL + 'todo/',
+        method: 'get',
+        headers: {
+            'Authorization': 'Token ' + token
+        },
+    }).then(function ({ data, status }) {
+        console.log(data);   
+        //filter the tasks based on the search input
+        const searchInput = document.getElementById('search-input').value;
+        const filteredTasks = data.filter(function (task) {
+            return task.title.toLowerCase().includes(searchInput.toLowerCase());
+        }
+        );
+        console.log(filteredTasks);
+        getSearchedTasks(filteredTasks);
+        displaySuccessToast('Tasks searched successfully');
+
+    }).catch(function (err) {
+        displayErrorToast('An error occured while getting the tasks');
+    })
+}
+
+
+    
