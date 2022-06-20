cacc526ade564298a6679da9994f6c5c069496fb
aayushimahajan <97786479+aayushimahajan@users.noreply.github.com>
2022-06-20 12:34:49 +0530

    Update auth_required.js

--- src/auth_required.js
+++ src/auth_required.js
@@ -1,6 +1,7 @@
 /***
  * @todo Redirect the user to login page if token is not present.
  */
- if (!localStorage.getItem('token')) {
+ var tokencheck=localStorage.getItem('token');
+ if (!tokencheck) {
     window.location.href = '/login/';
 }  
