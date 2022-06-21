e9953787a31132f9f81cabd7cbb304e665b19814
Anoushka Jha <95405559+BabyElias@users.noreply.github.com>
2022-06-22 00:36:42 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -220,8 +220,9 @@ function searchTask(){
     }).then(function ({ data, status }) {
         console.log(data);
         for (var j = 0; j < data.length; j++) if (data[j].title == taskforSearch){
-            taskList.innerHTML = "";
             displaySuccessToast("Task found");
+            document.getElementById('tasksList').innerHTML = " ";
+            
             
             return;
         }
