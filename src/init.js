b9480e9f1302780ba70f26715981be94ca6f51bb
jayantpranjal0 <jayant.pranjal.cse21@itbhu.ac.in>
2022-07-04 02:30:14 +0530

    Completed

--- src/init.js
+++ src/init.js
@@ -5,20 +5,41 @@ function getTasks() {
     /***
      * @todo Fetch the tasks created by the user and display them in the dom.
      */
-    const username=localStorage.getItem('token');
-    console.log(username);
-    //  axios({
-    //     headers: {
-    //         Authorization: 'Token ' + localStorage.getItem('token'),
-    //     },
-    //     url: API_BASE_URL + 'auth/'+username+'/',
-    //     method: 'get',
-    // }).then(function({data, status}) {
-    //   document.getElementById('avatar-image').src = 'https://ui-avatars.com/api/?name=' + data.name + '&background=fff&size=33&color=007bff';
-    //   document.getElementById('profile-name').innerHTML = data.name;
-    //   getTasks();
-    // })
-}
+     const tasks = document.getElementById('task');
+    axios({
+        method:'get',
+        headers:{
+            Authorization:`Token ${localStorage.getItem('token')}`
+        },
+        url:API_BASE_URL+"todo/"
+    }).then(({data,status})=>{
+            tasks.innerHTML=` <span class="badge badge-primary badge-pill todo-available-tasks-text">
+            Available Tasks
+        </span>`;
+            Array.from(data).forEach((ele)=>{
+                tasks.innerHTML+=`<li class="list-group-item d-flex justify-content-between align-items-center itemList">
+                <input id="input-button-${ele.id}" type="text" class="form-control todo-edit-task-input hideme" placeholder="Edit The Task">
+                <div id="done-button-${ele.id}"  class="input-group-append hideme">
+                    <button class="btn btn-outline-secondary todo-update-task" type="button" onclick="updateTask(${ele.id})">Done</button>
+                </div>
+                <div id="task-${ele.id}" class="todo-task todoTask">
+                    ${ele.title}
+                </div>
+                <span id="task-actions-${ele.id}">
+                    <button style="margin-right:5px;" type="button" onclick="editTask(${ele.id})"
+                        class="btn btn-outline-warning">
+                        <img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486663/CSOC/edit.png"
+                            width="18px" height="20px">
+                    </button>
+                    <button type="button" class="btn btn-outline-danger" onclick="deleteTask(${ele.id})">
+                        <img src="https://res.cloudinary.com/nishantwrp/image/upload/v1587486661/CSOC/delete.svg"
+                            width="18px" height="22px">
+                    </button>
+                </span>
+        </li>
+`
+            });
+        })}
 
 axios({
     headers: {
